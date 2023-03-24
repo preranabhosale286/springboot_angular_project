f26190d7075bfe32be24c2169c54af957dc8017a
preranabhosale286 <115148521+preranabhosale286@users.noreply.github.com>
2023-03-24 07:44:35 +0530

    Add files via upload

---
+++ Inventory-Management-System-main/airbus-management-spring/src/main/java/com/airbus/management/AirbusManagementSpringApplication.java
@@ -0,0 +1,10 @@
+package com.airbus.management;
+
+@SpringBootApplication
+public class AirbusManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbusManagementSpringApplication.class, args);
+	}
+
+}
